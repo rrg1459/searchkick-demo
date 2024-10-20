3a8473f6674f6cbfbf207d818ca4b70fdb2940b7
rafaDev <rafa.rodriguez@skydropx.com>
2024-10-20 09:43:10 -0400

    feat: personalze modelo Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  searchkick word_start: [:title, :author, :genre]
+
+  def search_data
+    {
+      title: title,
+      author: author,
+      genre: genre
+    }
+  end
 end
